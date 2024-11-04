1215ee747e22f2a1ae13bee23e15916849f802bc
lucas gomes <theminimare4514@gmail.com>
2024-11-04 19:49:30 -0300

    Adição de seleção de tipos de heróis, nome e idades

--- index.js
+++ index.js
@@ -31,3 +31,46 @@ class character {
         console.log(`o ${this.type} atacou usando ${this.weapon}`)
     }
 }
+
+let name = prompt("Digite o nome do herói: ")
+let age = parseInt(prompt("Digite a idade do herói: "))
+
+let option;
+
+do {
+    console.log("Escolha uma dos tipos de personagens")
+    console.log("Para escolhar, digite o número a direita do tipo")
+    console.log("1 - Guerreiro")
+    console.log("2 - Mago")
+    console.log("3 - Monge")
+    console.log("4 - Ninja")
+
+    option = parseInt(prompt(""))
+
+    if((option < 1 || option > 4)){
+        console.log("Digite uma opção válida!\n")
+        continue
+    }
+
+    break
+} while(true)
+
+let typeData
+
+switch(option){
+    case 1:
+        typeData = charTypes.warrior
+    break
+    case 2:
+        typeData = charTypes.mage
+    break
+    case 3:
+        typeData = charTypes.monk
+    break
+    case 4:
+        typeData = charTypes.ninja
+    break
+}
+
+let hero = new character(name, age, typeData)
+hero.attack()
